c1c0cfbd1ab9c36ea8fb3317174ef67680c0a495
arya1411 <patelarya424@gmail.com>
2026-08-31 16:44:10 +0530

    Smallest change ever:

--- Frontend/src/utils/apiPath.js
+++ Frontend/src/utils/apiPath.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
+export const BASE_URL = 'https://ai-interview-preparation-uw13.onrender.com'
 
 export const API_PATH = {
     AUTH : {
